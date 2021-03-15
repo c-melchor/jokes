62bdd9c1db2ea9bdd205294d95d4a357e9058fc4
c_melchor <cah.melchor@gmail.com>
2021-03-15 15:43:58 -0700

    tailwind

---
+++ src/components/Jokes.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function Jokes() {
+    const [joke, setJoke] = useState([])
+
+    useEffect(() => {
+        axios.get("https://official-joke-api.appspot.com/random_joke")
+            .then(res => { setJoke(res.data) })
+            .catch(err => { console.log(err) })
+    }, [])
+
+    const newJoke = () => {
+        window.location.reload(false)
+    }
+
+    return (
+        <div className="bg-grey-200 font-bold flex flex-col justify-center font-arial text-black text-center h-screen">
+            <p className="text-5xl pb-5"> Jokes!<span role="img" aria-label="laughing smiley face" className="pl-1.5">🤣 </span></p>
+            <div key={joke.id}>
+                <p className="text-3xl py-1">{joke.setup}</p>
+                <p className="text-xl py-1">{joke.punchline}</p>
+                <button onClick={newJoke} className="border-black py-2 px-2 mt-10 bg-red-500 text-white rounded">Make me laugh, clown</button>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <div className="flex bg-red-400 w-screen py-2 align-text-bottom">
+            <div className="w-2/4">
+                <p className="text-xl pl-3 text-white">Christina Melchor</p>
+            </div>
+            <div className=" w-2/4">
+                <ul className="flex flex-row justify-around align-end text-white text-md">
+                    <li className="px-2 border-3">
+                        <i class="fab fa-github"></i>
+                        <a href="https://github.com/c-melchor" className="px-2" target="_blank">Github</a>
+                    </li>
+                    <li>
+                        <i class="fab fa-linkedin"></i>
+
+                        <a href="https://www.linkedin.com/in/camelchor/" className="px-2" target="_blank">LinkedIn</a>
+                    </li>
+                </ul>
+            </div>
+        </div >
+    )
+}
