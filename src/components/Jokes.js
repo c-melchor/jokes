ff253a6d599fa672510781131fc1cb67f25406af
c_melchor <cah.melchor@gmail.com>
2021-03-17 09:17:47 -0700

    dependency fix

--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -3,26 +3,18 @@ import axios from "axios";
 
 export default function Jokes() {
     const [joke, setJoke] = useState([])
-    const [isFetching, setIsFetching] = useState(true)
 
     useEffect(() => {
         axios.get("https://official-joke-api.appspot.com/random_joke")
             .then(res => { setJoke(res.data) })
-            .then(() => { setIsFetching(!isFetching) })
             .catch(err => { console.log(err) })
-    }, [isFetching])
+    }, [])
 
     const newJoke = () => {
         window.location.reload(false)
     }
 
-    return (isFetching ? <div className="bg-grey-200 font-bold flex flex-col justify-center align-center font-arial text-black text-center h-screen ">
-        <p className="text-5xl pb-5"> Jokes!<span role="img" aria-label="laughing smiley face" className="pl-1.5">🤣 </span></p>
-
-        <h2>...loading</h2>
-        <button onClick={newJoke} className="border-black m-auto mt-40 py-2 px-2 mt-10 bg-red-500 text-white rounded w-1/4">Make me laugh, clown</button>
-    </div> :
-
+    return (
         <div className="bg-grey-200 font-bold flex flex-col justify-center font-arial text-black text-center h-screen">
             <p className="text-5xl pb-5"> Jokes!<span role="img" aria-label="laughing smiley face" className="pl-1.5">🤣 </span></p>
             <div key={joke.id}>
